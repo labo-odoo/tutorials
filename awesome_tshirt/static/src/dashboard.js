80d28e9a752727bd612e0380c356271b6852c558
lakhichand borse <labo@odoo.com>
2023-10-03 18:44:03 +0530

    chapter 2

--- awesome_tshirt/static/src/dashboard.js
+++ awesome_tshirt/static/src/dashboard.js
@@ -1,11 +1,60 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+// import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
+import { getDefaultConfig } from "@web/views/view";
+const { Component, useSubEnv , useState } = owl;
+import { useService } from "@web/core/utils/hooks";
+import { Domain } from "@web/core/domain";
 
-class AwesomeDashboard extends Component {}
+class AwesomeDashboard extends Component {
 
-AwesomeDashboard.components = {};
+	setup(){
+		  useSubEnv({
+            config: {
+                ...getDefaultConfig(),
+                ...this.env.config,
+            },
+
+        });
+
+		this.display = {	
+            controlPanel: { "top-right": false, "bottom-right": false },
+        };  
+        this.action = useService("action");
+	}
+
+	openCustomerView() {
+        this.action.doAction("base.action_partner_form");
+    }
+
+    openOrders(title, domain) {
+        this.action.doAction({
+            type: "ir.actions.act_window",
+            name: title,
+            res_model: "awesome_tshirt.order",
+            domain: new Domain(domain).toList(),
+            views: [
+                [false, "list"],
+                [false, "form"],
+            ],
+        });
+    }
+    openLast7DaysOrders() {
+        const domain =
+            "[('create_date','>=', (context_today() - datetime.timedelta(days=7)).strftime('%Y-%m-%d'))]";
+        this.openOrders("Last 7 days orders", domain);
+    }
+
+    openLast7DaysCancelledOrders() {
+        const domain =
+            "[('create_date','>=', (context_today() - datetime.timedelta(days=7)).strftime('%Y-%m-%d')), ('state','=', 'cancelled')]";
+        this.openOrders("Last 7 days cancelled orders", domain);
+    }
+}
+
+AwesomeDashboard.components = {Layout};
 AwesomeDashboard.template = "awesome_tshirt.clientaction";
 
 registry.category("actions").add("awesome_tshirt.dashboard", AwesomeDashboard);
